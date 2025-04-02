4f11eae2fe32122ad50ec8cc38a57566dac41281
kkuzmov <kkuzmov@gmail.com>
2025-04-02 21:54:46 +0300

    feat: implement gallery component with search functionality and infinite scroll

--- src/client/scripts/components/fadeText.js
+++ src/client/scripts/components/fadeText.js
@@ -1,7 +1,1 @@
-window.onload = function () {
-  const title =
-    document.querySelector("h1 > span") || document.querySelector("h1");
-  setTimeout(function () {
-    title.style.opacity = 1;
-  }, 2000);
-};
+document.addEventListener("DOMContentLoaded", function () {});
