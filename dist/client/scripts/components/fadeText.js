b440f08f6e75854c8b7952e8fa1922d412c7c482
kkuzmov <kkuzmov@gmail.com>
2025-04-10 22:52:13 +0300

    feat: add fadeText component for title and subtitle fade-in effect

--- dist/client/scripts/components/fadeText.js
+++ dist/client/scripts/components/fadeText.js
@@ -1,1 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  fadeIn();
+
+  function fadeIn() {
+    const title =
+      document.querySelector("h1 > span") || document.querySelector("h1");
+    const subtitle = document.querySelector("h2");
+
+    setTimeout(function () {
+      title.style.opacity = 1;
+    }, 2000);
+
+    if (subtitle) {
+      setTimeout(function () {
+        subtitle.style.opacity = 1;
+        subtitle.style.height = "auto";
+      }, 2500);
+    }
+  }
+});
--- dist/client/scripts/components/gallery.js
+++ dist/client/scripts/components/gallery.js
@@ -180,25 +180,8 @@ document.addEventListener("DOMContentLoaded", function () {
       fetchArtworks();
     }
   });
-  fadeIn();
+
   setTimeout(() => {
     fetchArtworks();
   }, 3000);
-
-  function fadeIn() {
-    const title =
-      document.querySelector("h1 > span") || document.querySelector("h1");
-    const subtitle = document.querySelector("h2");
-
-    setTimeout(function () {
-      title.style.opacity = 1;
-    }, 2000);
-
-    if (subtitle) {
-      setTimeout(function () {
-        subtitle.style.opacity = 1;
-        subtitle.style.height = "auto";
-      }, 2500);
-    }
-  }
 });
